b07c6db2f618ea845dccb8f6b2ab10a1e8426574
Olesya Tretyakova <OTretyakova@beeline.kz>
2021-09-03 18:03:18 +0600

    add photo

--- src/test/java/com.my_company/tests/ToolsQATest.java
+++ src/test/java/com.my_company/tests/ToolsQATest.java
@@ -3,17 +3,16 @@ package com.my_company.tests;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 
-import java.io.File;
-
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class ToolsQATest {
     @BeforeAll
     static void beforeAll() {
         Configuration.startMaximized = true;
+        Configuration.timeout = 20000;
     }
 
     @Test
@@ -21,20 +20,41 @@ public class ToolsQATest {
         String firstName = "Tom";
         String lastName = "Cruise";
         String email = "tom_cruise@gmail.com";
-        String sexMale = "Male";
+        String sex = "Male";
         String userNumber = "77777777777";
-
+        String address = "USA";
+        String state = "NCR";
+        String city = "Noida";
+        String subject = "Math";
+        String day = "03";
+        String month = "July";
+        String year = "1962";
+        String hobby = "Sports";
+        String fileName = "photo.jpg";
         open("https://demoqa.com/automation-practice-form");
+
 
         $("#firstName").setValue(firstName);
         $("#lastName").setValue(lastName);
         $("#userEmail").setValue(email);
-        $("[for=gender-radio-1]").click();
-        $(".custom-control-input").selectRadio(sexMale);
+        $("#genterWrapper").$(byText(sex)).click();
         $("#userNumber").setValue(userNumber);
-        $("#dateOfBirthInput").setValue("03 Jul 1962");
-        $("#hobbies-checkbox-1").click();
-        $("#uploadPicture").uploadFile(new File("C:\\Users\\OTretyakova\\Pictures\\test_pic.jpg"));
+        $(".react-datepicker__month-select").click();
+        $(".react-datepicker__month-select").$(byText(month)).click();
+        $(".react-datepicker__year-select").click();
+        $(".react-datepicker__year-select").$(byText(year)).click();
+        $(byText(day)).click();
+        $("#subjectsInput").setValue(subject).pressEnter();
+        $("#hobbiesWrapper").$(byText(hobby)).click();
+        $("#uploadPicture").uploadFromClasspath(fileName);
+        $("#currentAddress").setValue(address);
+
+        $("#stateCity-wrapper").$(byText("Select State")).scrollIntoView(true).click();
+        $("#state").$(byText(state)).click();
+        $("#city").click();
+        $("#city").$(byText(city)).click();
+        $("#submit").click();
+        $("#submit").click();
 
     }
 
